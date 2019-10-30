43e4520fbfbbe01dd71a1ed91be88dddd621a477
Romain DALICHAMP <romain.dalichamp@free.fr>
2019-10-30 11:57:37 -0400

    Crud Legacy Servlet Project

---
+++ Java/CrudLegacyServlet/target/classes/init-hibernate.sql
@@ -0,0 +1,4 @@
+insert into User (id, firstname, lastname) values (1, 'Romain', 'DALICHAMP');
+insert into User (id, firstname, lastname) values (2, 'Morgan', 'Doe');
+insert into User (id, firstname, lastname) values (3, 'Thierno', 'Pierre');
+insert into User (id, firstname, lastname) values (4, 'Jeremi', 'Dupont');
