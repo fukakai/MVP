c2d3cf508eaa772d5c812f93d1774e2601325571
Romain DALICHAMP <romain.dalichamp@gmail.com>
2019-12-03 15:46:32 -0500

    First Commit

--- Spring/Microservices-with-Spring-Boot/SpringEurekaAuth/src/main/java/fr/dalichamp/microservices/auth/SpringEurekaAuthApplication.java
+++ Spring/Microservices-with-Spring-Boot/SpringEurekaAuth/src/main/java/fr/dalichamp/microservices/auth/SpringEurekaAuthApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class SpringEurekaAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringEurekaAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaAuthApplication.class, args);
+    }
 
 }
