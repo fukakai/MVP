765930d81b2bf064ad785f34beb58f638055a7a3
Romain DALICHAMP <romain.dalichamp@gmail.com>
2019-12-03 16:48:44 -0500

    second commit

---
+++ Spring/springvault/src/main/java/com/alithya/mvp/springvault/SpringvaultApplication.java
@@ -0,0 +1,13 @@
+package com.alithya.mvp.springvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvaultApplication.class, args);
+	}
+
+}
---
+++ Spring/springvault/src/test/java/com/alithya/mvp/springvault/SpringvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alithya.mvp.springvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
