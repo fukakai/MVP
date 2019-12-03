1a73e0ce7b4de76c3a3a89bd5687543a36b07618
Romain DALICHAMP <romain.dalichamp@gmail.com>
2019-12-03 18:44:37 -0500

    Vault Configuration

--- Spring/springvault/src/main/java/com/alithya/mvp/springvault/SpringvaultApplication.java
+++ Spring/springvault/src/main/java/com/alithya/mvp/springvault/SpringvaultApplication.java
@@ -1,9 +1,12 @@
 package com.alithya.mvp.springvault;
 
+import com.alithya.mvp.springvault.config.VaultConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(VaultConfiguration.class)
 public class SpringvaultApplication {
 
 	public static void main(String[] args) {
